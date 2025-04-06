a4258f3cabff6921d58ff4f58bf54f4ec518a0b8
xKatyJane <katarzyna.hodiak@gmail.com>
2025-04-07 00:03:02 +0200

    Destination and departure added

--- FlightHawkBot.py
+++ FlightHawkBot.py
@@ -14,18 +14,18 @@ date_choice = None
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-FLIGHT_TYPE, ORIGIN_MAIN, ORIGIN_SPECIFIC, DESTINATION, DATE = range(5)
+FLIGHT_TYPE, ORIGIN_PRELIMINARY, ORIGIN_MAIN, ORIGIN_SPECIFIC, DESTINATION_PRELIMINARY, DESTINATION_MAIN, DESTINATION_SPECIFIC, DATE = range(8)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    ''' Displays the welcome message and asks for flight type '''
     user = update.effective_user
     reply_keyboard = [["One way", "Return", "Multi-city"]]
 
     await update.message.reply_text(
-        '<b>Welcome to the Flight Hawk Bot!\n'
+        '✈️ <b>Welcome to the Flight Hawk Bot! ✈️\n'
         'I can help you to find cheap flights and track fare changes.\n</b>'
         'What type of flight are you looking for?\n',
         parse_mode='HTML',
@@ -41,62 +41,132 @@ async def flight_type(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     flight_type_choice = update.message.text
     logger.info(f"User {user.first_name} chose: {flight_type_choice}")
     context.user_data["flight_type_choice"] = flight_type_choice
-    #FlightTracker().origin_main(context)
-#    flight_tracker = FlightTracker.FlightTracker(context)
-    await update.message.reply_text(f"You selected: {flight_type_choice}. What is your departure city?")
-    return ORIGIN_MAIN
+    await update.message.reply_text(f"{flight_type_choice} it is! 🛫 What is your departure city?")
+    return ORIGIN_PRELIMINARY
 
 async def choose_origin_main(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     ''' Saves the departure city '''
 
     user = update.message.from_user
-    origin_main_choice = update.message.text
-    logger.info(f"User {user.first_name} chose: {origin_main_choice}")
-    context.user_data["origin_main_choice"] = origin_main_choice
+    origin_preliminary_choice = update.message.text
+    logger.info(f"User {user.first_name} entered the departure city as: {origin_preliminary_choice}")
+    context.user_data["origin_preliminary_choice"] = origin_preliminary_choice
     flight_type_choice = context.user_data.get("flight_type_choice", "Default Value")
     flight_tracker = FlightTracker.FlightTracker(context)
-#    flight_tracker = FlightTracker.FlightTracker(context)
-#    main_origin_options = flight_tracker.origin_main()
     flight_tracker.get_google_flights()
-    main_origin_options = flight_tracker.origin_main(context)
+    flight_tracker.flight_type(context)
+    main_origin_options = flight_tracker.main_choice(context, 1, origin_preliminary_choice)
 
     if main_origin_options:
 #        reply_keyboard = [[KeyboardButton(text=option)] for option in main_origin_options]
         reply_keyboard = [[option] for option in main_origin_options]
-        await update.message.reply_text("Please choose the city from the list.",
+        await update.message.reply_text("📌 Choose a departure option from the list.",
                                         reply_markup=ReplyKeyboardMarkup(
                                             reply_keyboard,
                                             one_time_keyboard=True,
                                             resize_keyboard=True))
     else:
         await update.message.reply_text("No options found, please try again.")
-    return ORIGIN_SPECIFIC
+    return ORIGIN_MAIN
 
 async def choose_origin_specific(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.message.from_user
-    origin_specific_choice = update.message.text
-    logger.info(f"User {user.first_name} chose: {origin_specific_choice}")
-    context.user_data["origin_specific_choice"] = origin_specific_choice
+    origin_main_choice = update.message.text
+    logger.info(f"User {user.first_name} chose a main origin option: {origin_main_choice}")
+    context.user_data["origin_main_choice"] = origin_main_choice
     flight_tracker = FlightTracker.FlightTracker(context)
-    flight_tracker.origin_specific(context)
+    flight_tracker.specific_choice(context, origin_main_choice)
 
-    origin_specific_options = flight_tracker.origin_specific(context)
+    origin_specific_options = flight_tracker.specific_choice(context, origin_main_choice)
     if len(origin_specific_options)<=2:
-        pass
+        context.user_data["origin_specific_choice"] = origin_main_choice
+        logger.info(f"User {user.first_name} selected an origin main with only one option: {origin_main_choice}, so not asking for specific origin.")
+        await update.message.reply_text(f"Great! What is your destination city? 🛬")
+        return DESTINATION_PRELIMINARY
     elif len(origin_specific_options)>2:
         reply_keyboard = [[option] for option in origin_specific_options]
-        await update.message.reply_text("You can choose an airport or the city to search for all airports.",
+        await update.message.reply_text("You can choose an airport or the main for all airports. ✈️",
                                         reply_markup=ReplyKeyboardMarkup(
                                             reply_keyboard,
                                             one_time_keyboard=True,
                                             resize_keyboard=True))
         print(len(origin_specific_options))
+        return ORIGIN_SPECIFIC
     else:
         await update.message.reply_text("No options found, please try again.")
-    return DESTINATION
+        return ORIGIN_SPECIFIC
 
 async def choose_departure_main(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.message.from_user
+    origin_specific_choice = update.message.text
+    logger.info(f"User {user.first_name} chose origin specific as: {origin_specific_choice}")
+    context.user_data["origin_specific_choice"] = origin_specific_choice
+    await update.message.reply_text(f"Great! What is your destination city? 🛬")
+    return DESTINATION_PRELIMINARY
+
+async def choose_destination_main(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.message.from_user
+    destination_preliminary_choice = update.message.text
+    logger.info(f"User {user.first_name} entered the destination city as: {destination_preliminary_choice}")
+    context.user_data["destination_preliminary_choice"] = destination_preliminary_choice
+    flight_tracker = FlightTracker.FlightTracker(context)
+    origin_specific_choice = context.user_data.get("origin_specific_choice")
+    flight_tracker.confirm_decision(context, origin_specific_choice)
+#    flight_type_choice = context.user_data.get("flight_type_choice", "Default Value")
+    main_destination_options = flight_tracker.main_choice(context, 2, destination_preliminary_choice)
+    print(main_destination_options)
+
+    if main_destination_options:
+        reply_keyboard = [[option] for option in main_destination_options]
+        await update.message.reply_text("📌 Choose a destination option from the list.",
+                                        reply_markup=ReplyKeyboardMarkup(
+                                            reply_keyboard,
+                                            one_time_keyboard=True,
+                                            resize_keyboard=True))
+    else:
+        await update.message.reply_text("No options found, please try again.")
+    return DESTINATION_MAIN
+
+async def choose_destination_specific(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.message.from_user
+    destination_main_choice = update.message.text
+    logger.info(f"User {user.first_name} chose a main destination option: {destination_main_choice}")
+    context.user_data["destination_main_choice"] = destination_main_choice
+    flight_tracker = FlightTracker.FlightTracker(context)
+
+    destination_specific_options = flight_tracker.specific_choice(context, destination_main_choice)
+    if len(destination_specific_options)<=2:
+        context.user_data["destination_specific_options"] = destination_main_choice
+        logger.info(f"User {user.first_name} selected a destination main with only one option: {destination_main_choice}, so not asking for specific destination.")
+        await update.message.reply_text(f"Noted! What is the departure date? For example DD-MM-YYYY.")
+#        return DESTINATION_PRELIMINARY
+
+    elif len(destination_specific_options)>2:
+        reply_keyboard = [[option] for option in destination_specific_options]
+        await update.message.reply_text("You can choose an airport or the city to search for all airports. ✈️",
+                                        reply_markup=ReplyKeyboardMarkup(
+                                            reply_keyboard,
+                                            one_time_keyboard=True,
+                                            resize_keyboard=True))
+        print(len(destination_specific_options))
+    else:
+        await update.message.reply_text("No options found, please try again.")
+    return DESTINATION_SPECIFIC
+
+async def choose_date(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.message.from_user
+    destination_specific_choice = update.message.text
+    logger.info(f"User {user.first_name} chose destination specific as: {destination_specific_choice}")
+    context.user_data["destination_specific_choice"] = destination_specific_choice
+    flight_tracker = FlightTracker.FlightTracker(context)
+    destination_specific_choice = context.user_data.get("destination_specific_choice")
+    flight_tracker.confirm_decision(context, destination_specific_choice)
+    await update.message.reply_text(f"Noted! What is the departure date? For example DD-MM-YYYY.")
+    return DATE
+
+async def confirmed(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     pass
+
 
 
 def main() -> None:
@@ -107,9 +177,13 @@ def main() -> None:
         entry_points=[CommandHandler('start', start)],
         states={
             FLIGHT_TYPE: [MessageHandler(filters.Text(["One way", "Return", "Multi-city"]), flight_type)],
-            ORIGIN_MAIN: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_origin_main)],
-            ORIGIN_SPECIFIC: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_origin_specific)],
-            DESTINATION: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_departure_main)],
+            ORIGIN_PRELIMINARY: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_origin_main)],
+            ORIGIN_MAIN: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_origin_specific)],
+            ORIGIN_SPECIFIC: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_departure_main)],
+            DESTINATION_PRELIMINARY: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_destination_main)],
+            DESTINATION_MAIN: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_destination_specific)],
+            DESTINATION_SPECIFIC: [MessageHandler(filters.TEXT & ~filters.COMMAND, choose_date)],
+            DATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, confirmed)],
         },
         fallbacks=[],
     )
--- FlightTracker.py
+++ FlightTracker.py
@@ -13,16 +13,31 @@ class FlightTracker():
 
     def __init__(self, context):
         self.context = context
+
+        if "driver" in context.user_data:
+            self.driver = context.user_data["driver"]
+        else:
+            options = webdriver.ChromeOptions()
+            self.service = Service(executable_path="/Users/katia/Selenium_Chromedriver/chromedriver")
+            self.driver = webdriver.Chrome(service=self.service, options=options)
+            context.user_data["driver"] = self.driver
+
+
+
         self.flight_type_choice = context.user_data.get("flight_type_choice", None)
+        self.origin_preliminary_choice = context.user_data.get("origin_preliminary_choice", None)
         self.origin_main_choice = context.user_data.get("origin_main_choice", None)
         self.origin_specific_choice = context.user_data.get("origin_specific_choice", None)
 #        self.grouped_origin_options = context.user_data['grouped_origin_options', None]
-        self.driver = None
+#        self.driver = None
 #        options = webdriver.ChromeOptions()
 #        options.add_argument("--headless")
 #        self.driver = webdriver.Chrome(service=self.service)
 #        self.service = Service(executable_path="/Users/katia/Selenium_Chromedriver/chromedriver")
 #        self.driver.get("https://www.google.com/travel/flights")
+        
+
+
         
         # Functions
 
@@ -33,28 +48,30 @@ class FlightTracker():
 
     def get_google_flights(self):
 
-        options = webdriver.ChromeOptions()
-        self.service = Service(executable_path="/Users/katia/Selenium_Chromedriver/chromedriver")
-        self.driver = webdriver.Chrome(service=self.service)
-        self.driver.get("https://www.google.com/travel/flights")
+#        options = webdriver.ChromeOptions()
+#        self.service = Service(executable_path="/Users/katia/Selenium_Chromedriver/chromedriver")
+#        self.driver = webdriver.Chrome(service=self.service)
+        if self.driver.current_url != "https://www.google.com/travel/flights":
+            self.driver.get("https://www.google.com/travel/flights")
 
-        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "(//div[@class='lssxud']//button)[2]")))
-        button = self.driver.find_element(By.XPATH, "(//div[@class='lssxud']//button)[2]")
-        button.click()
-        
-    def origin_main(self, context):
-        global grouped_origin_options, main_origin_options
+            WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "(//div[@class='lssxud']//button)[2]")))
+            button = self.driver.find_element(By.XPATH, "(//div[@class='lssxud']//button)[2]")
+            button.click()
 
+    def flight_type(self, context):
         # Flight type: one way, return, multi-city
         flight_type_choice = context.user_data.get("flight_type_choice", "Default Value")
         if flight_type_choice == "One Way":
             print(flight_type_choice)
 
+    def main_choice(self, context, input_box, input_value):
+        global grouped_origin_options, main_origin_options
+
         # Input: origin
-        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "(//div[contains(@class, 'wUiEcc')]/div/div/div/input)[1]")))
-        origin_input = self.driver.find_element(By.XPATH, "(//div[contains(@class, 'wUiEcc')]/div/div/div/input)[1]")
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, f"(//div[contains(@class, 'wUiEcc')]/div/div/div/input)[{input_box}]")))
+        origin_input = self.driver.find_element(By.XPATH, f"(//div[contains(@class, 'wUiEcc')]/div/div/div/input)[{input_box}]")
         origin_input.clear()
-        origin_input.send_keys(self.origin_main_choice)
+        origin_input.send_keys(input_value)
 
         # Open buttons to see details of each location
         WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "//ul[@class='DFGgtd']//button")))
@@ -65,7 +82,7 @@ class FlightTracker():
         # Collecting origin options
         origin_options = []
 #        options_list = self.driver.find_elements(By.XPATH, "//ul[@class='DFGgtd']/li//div[@class='zsRT0d']")
-        options_list = self.driver.find_elements(By.XPATH, "//div[@class='CwL3Ec']/div[1]")
+        options_list = self.driver.find_elements(By.XPATH, f"//div[@class='CwL3Ec']/div[1]")
         for i in options_list:
             origin_options.append(i.text)
 
@@ -76,7 +93,7 @@ class FlightTracker():
         # Finding the main destinations (contain ",") and airports (contain 3 capitalized letters in the end)
         pattern = r"[A-Za-z\s]+([A-Z]{3})$"
         for option in origin_options:
-            if "," in option:
+            if option == origin_options[0] or "," in option:
                 current_main_origin = option
                 grouped_origin_options[current_main_origin] = []
             elif re.search(pattern, option):
@@ -88,22 +105,28 @@ class FlightTracker():
 
         return main_origin_options
 
-    def origin_specific(self, context):
-
-        origin_specific_choice = context.user_data.get("origin_specific_choice", "Default Value")
-        print(origin_specific_choice)
+    def specific_choice(self, context, main_choice):
+        specific_options = []
         for key in grouped_origin_options:
-            if key == origin_specific_choice:
-                origin_specific_options = [key] + grouped_origin_options.get(origin_specific_choice, [])
-        print(origin_specific_options)
-        return origin_specific_options
-#        chosen_main_origin = main_origin_options[1]
+            if key == main_choice:
+                specific_options = [key] + grouped_origin_options.get(main_choice, [])
+        return specific_options
 
-#        origin_specific_options = [chosen_main_origin]
-#        for sub in grouped_origin_options[chosen_main_origin]:
-#            origin_specific_options.append(sub)
-        
-#        print(origin_specific_options)
+    def confirm_decision(self, context, specific_choice):
+    #    origin_specific_choice = context.user_data.get("origin_specific_choice", "Default Value")
+
+        pattern = r"[A-Za-z\s]+([A-Z]{3})$"
+        if re.search(pattern, specific_choice):
+            specific_choice_search = specific_choice[:-5]
+        else: 
+            specific_choice_search = specific_choice
+        print(specific_choice_search)
+
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//*[contains(text(), '{specific_choice_search}')]")))
+        origin_specific_choice_element = self.driver.find_element(By.XPATH, f"//*[contains(text(), '{specific_choice_search}')]")
+        origin_specific_choice_element.click()
+
+
 
 
 
